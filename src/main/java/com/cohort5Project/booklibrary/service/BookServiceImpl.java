be38ab422b61d851ab418a9cab8d3be5227fd196
Daniel Idoko <goldann.c@gmail.com>
2021-06-21 19:45:44 +0100

    Fixed local configuration

--- src/main/java/com/cohort5Project/booklibrary/service/BookServiceImpl.java
+++ src/main/java/com/cohort5Project/booklibrary/service/BookServiceImpl.java
@@ -2,11 +2,14 @@ package com.cohort5Project.booklibrary.service;
 
 import com.cohort5Project.booklibrary.entity.Book;
 import com.cohort5Project.booklibrary.repository.BookRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service
+@Slf4j
 public class BookServiceImpl implements BookService {
 
     private BookRepository bookRepository;
@@ -41,4 +44,32 @@ public class BookServiceImpl implements BookService {
        bookRepository.deleteById(id);
 
     }
+
+
+    @PostConstruct
+    public void populateRepository() {
+
+        Book bookOne = new Book("THINK AND GROW RICH", "NAPOLEON HILL", "The book asserts that desire, faith, and persistence can propel one to great heights if one can", "1937");
+        bookRepository.save(bookOne);
+
+        Book bookTwo = new Book("THE GIFTED HANDS", "BEN CARSON", "This book outlines his childhood and adulthood, the joys and trials that he experienced led him to", "1992");
+        bookRepository.save(bookTwo);
+
+        Book bookThree = new Book("THINGS FALL APART", "CHINUA ACHEBE", "The book asserts that desire, faith, and persistence can propel one to great heights if one ca", "1958");
+        bookRepository.save(bookThree);
+
+        Book bookFour = new Book("THE MAN DIED!", "WOLE SHOYINKA", "This book outlines his childhood and adulthood, the joys and trials that he experienced led him to", "1972");
+        bookRepository.save(bookFour);
+
+        Book bookFive = new Book("RICH DAD POOR DAD", "ROBERT KIYOSAKI ", "The book asserts that desire, faith, and persistence can propel one to great heights if one ", "1997");
+        bookRepository.save(bookFive);
+        log.info("Populated ");
+    }
+
+
+
+
+
+
+
 }
